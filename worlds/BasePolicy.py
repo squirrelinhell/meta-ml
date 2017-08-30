277d61596a5aef8b944fbbce58a26d4e612e2ec7
SquirrelInHell <squirrelinhell@users.noreply.github.com>
2017-08-30 19:44:03 +0200

    Fix policy code

--- worlds/BasePolicy.py
+++ worlds/BasePolicy.py
@@ -17,41 +17,47 @@ class BasePolicy(World):
         def _run_episodes(seed_batch, eval_batch, reward_batch):
             # Start <batch_size> episodes in parallel
             eps = [world.start_episode(s) for s in seed_batch]
-            is_active = np.array([True] * len(eps))
-            reward_sum = np.zeros(self.r_shape)
+            is_active = [True] * len(eps)
+            history = []
+            rews = [[] for _ in eps]
 
             # Gather rewards until all episodes are finished
-            while is_active.any():
-                active = []
-                inps = []
+            while True:
+                active_i = []
+                o_batch = []
                 for i in range(len(eps)):
                     if is_active[i]:
                         try:
-                            inps.append(eps[i].get_observation())
-                            active.append(i)
+                            o = eps[i].get_observation()
+                            o_batch.append(o)
+                            active_i.append(i)
                         except StopIteration:
                             is_active[i] = False
 
-                if len(active) < 1:
+                if len(active_i) < 1:
                     break
 
-                outs = eval_batch(inps)
-                rews = [eps[i].step(o) for i, o in zip(active, outs)]
+                a_batch = eval_batch(o_batch)
+                assert a_batch.shape == (len(active_i),) + world.a_shape
 
-                out_rews = reward_batch(inps, outs, rews)
-                assert out_rews.shape == self.r_shape
-                reward_sum += out_rews * len(inps)
+                for o, a, i in zip(o_batch, a_batch, active_i):
+                    r = eps[i].step(a)
+                    rews[i].append(r)
+                    history.append((o, a, i))
 
-            reward_sum /= batch_size
+            # Averaging rewards from each episode will in the end
+            # work like summing, because they appear multiple times
+            rews = np.array([np.mean(r, axis=0) for r in rews])
+            assert rews.shape == (batch_size,) + world.r_shape
 
             # Normalize rewards if requested
             if normalize_rewards:
-                reward_sum -= reward_sum.mean()
-                stddev = reward_sum.std()
-                if stddev < 0.00001:
-                    return np.zeros(reward_sum.shape)
-                reward_sum /= stddev
+                rews -= rew.mean()
+                stddev = rews.std()
+                assert stddev > 0.00001
+                rews /= stddev
 
-            return reward_sum
+            history = [(o, a, rews[i]) for o, a, i in history]
+            return reward_batch(*zip(*history))
 
         self._run_episodes = _run_episodes
